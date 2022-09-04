5bd913e46a82b1f521f8c4c19e67408b3ce03a44
GonzaGalfre <gonzaa.galfre@gmail.com>
2022-09-04 17:18:10 -0300

    Update

--- homebanking/clientes/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Cliente
-
-
-def index(request):
-    # clients = Cliente.objects.all()
-    return render(request, 'clientes/index.html')
--- homebanking/login/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views as core_views
-
-app_name = 'login'
-urlpatterns = [
-    path('signup/', core_views.signup, name='signup'),
-]
