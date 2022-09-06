adcfda07c9a4ecf178287823b1e1f11aae9535e6
GonzaGalfre <gonzaa.galfre@gmail.com>
2022-09-06 15:10:25 -0300

    Update urls

--- sprint7/login/urls.py
+++ sprint7/login/urls.py
@@ -4,6 +4,5 @@ from . import views
 app_name = 'login'
 urlpatterns = [
     path('', views.signup, name='signup'),
-    path('login2/', views.login2, name='login2'),
     path('signout/', views.signout, name='signout')
 ]
